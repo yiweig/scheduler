0335fd8651e96655398929f1491a26e27e24f989
Yiwei Gao <ygao93@gmail.com>
2016-05-17 15:19:19 -0400

    add __unicode__() to data models

--- scheduler/scraper/models.py
+++ scheduler/scraper/models.py
@@ -5,12 +5,18 @@ class Department(models.Model):
     full_name = models.CharField(max_length=128)
     abbreviation = models.CharField(max_length=32)
 
+    def __unicode__(self):
+        return '%s (%s)', self.full_name, self.abbreviation
+
 
 class Instructor(models.Model):
     # a department can have many instructors
     department = models.ForeignKey(to=Department, on_delete=models.SET_NULL)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
+
+    def __unicode__(self):
+        return '%s %s', self.first_name, self.last_name
 
 
 class Course(models.Model):
@@ -19,16 +25,26 @@ class Course(models.Model):
     course_number = models.CharField(max_length=8)
     course_name = models.CharField(max_length=128)
 
+    def __unicode__(self):
+        return '%s %s %s', self.department, self.course_number, self.course_name
+
 
 class Section(models.Model):
     # a course can have many sections
     course = models.ForeignKey(to=Course, on_delete=models.CASCADE)
+    section_number = models.CharField(max_length=8)
+    location = models.CharField(max_length=128)
     # an instructor can have many sections, and a section can have many instructors
     instructor = models.ManyToManyField(to=Instructor)
-    location = models.CharField(max_length=128)
+
+    def __unicode__(self):
+        return '%s %s %s', self.section_number, self.location, self.instructor
 
 
 class TimeSlot(models.Model):
     # a section can have many timeslots
     section = models.ForeignKey(to=Section, on_delete=models.CASCADE)
     duration = models.DurationField()
+
+    def __unicode__(self):
+        return '%s', self.duration
