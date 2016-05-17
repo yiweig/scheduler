acda5fe51c38532fbd8dc504a84c222aa5e895f8
Yiwei Gao <ygao93@gmail.com>
2016-05-17 19:55:39 -0400

    added views, but still broken

--- scheduler/scraper/urls.py
+++ scheduler/scraper/urls.py
@@ -3,5 +3,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ex: /scraper/
     url(r'^$', views.index, name='index'),
+    # ex: /scraper/5/
+    url(r'^(?P<scraper_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /scraper/5/results/
+    url(r'^(?P<scraper_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /scraper/5/vote/
+    url(r'^(?P<scraper_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
 ]
--- scheduler/scraper/views.py
+++ scheduler/scraper/views.py
@@ -1,5 +1,30 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Department
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the scraper index.")
+    department_list = Department.objects.order_by('abbreviation')
+    # template = loader.get_template('scraper/index.html')
+    context = {'department_list': department_list}
+
+    # output = ', '.join([d.abbreviation for d in department_list])
+
+    # return HttpResponse(template.render(context=context, request=request))
+    return render(request=request, template_name='scraper/index.html', context=context)
+
+
+def detail(request, department_id):
+    department = get_object_or_404(Department, pk=department_id)
+    context = {'department': department}
+    return render(request=request, template_name='scraper/detail.html', context=context)
+
+
+def results(request, department_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % department_id)
+
+
+def vote(request, department_id):
+    return HttpResponse("You're voting on question %s." % department_id)
